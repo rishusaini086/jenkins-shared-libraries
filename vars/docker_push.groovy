a2440c95371c2a407e099d86ab019b4983e4d9e9
rishusaini086 <rishusaini086@gmail.com>
2026-02-22 03:53:37 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String dockerUser, String image, String tag){
   withCredentials([usernamePassword(credentialsId:"docker-creds",usernameVariable:"dockerUser",passwordVariable:"dockerPass")]){
-    sh "docker login -u ${dockerUser} -p ${dockerPass}
+    sh "docker login -u ${dockerUser} -p ${dockerPass}"
   }
     sh "docker push ${dockerUser}/${image}:${tag}"
 }
